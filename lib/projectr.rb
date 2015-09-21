a55157734ae4521928c8049e76694384cfc448ac
Josh Glendenning <joshglendenning@gmail.com>
2015-09-21 13:40:33 -0500

    ~

--- lib/projectr.rb
+++ lib/projectr.rb
@@ -1,36 +1,14 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'projectr/version'
-require 'commander'
 require 'fileutils'
 
 module Projectr
-	def self.run
-		include Commander::Methods
-		program :name, 'Projectr'
-		program :version, Projectr::VERSION
-		program :description, 'Project templating tool for quickly starting and updating new projects.'
-
-		command :list do |c|
-			c.syntax = 'list [options]'
-			c.action do |args, opts|
-				Projectr.list()
-			end
-		end
-
-		command :load do |c|
-			c.syntax = 'load <name> [options]'
-			c.action do |args, opts|
-				Projectr.load(args.first)
-			end
-		end
-	end
-
 	PROJECT_PATH = "#{Dir.home}/.projectr"
 	def self.list
 		dirs = Dir.glob("#{PROJECT_PATH}/*")
-		.select { |f|File.directory? f }
-		.map { |p| p.split('/').last }
+			.select { |f|File.directory? f }
+			.map { |p| p.split('/').last }
 		dirs.each { |dir| puts dir }
 	end
 	def self.load(name, dest_path = Dir.pwd)
