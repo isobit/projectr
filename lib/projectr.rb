2170d4b18699c8c4524a28c234f140dd5d03d2d1
Josh Glendenning <joshglendenning@gmail.com>
2015-09-21 12:08:19 -0500

    ~

--- lib/projectr.rb
+++ lib/projectr.rb
@@ -2,7 +2,7 @@ require 'projectr/version'
 require 'json'
 
 module Projectr
-	PROJECT_PATH = "~/.projectr"
+	PROJECT_PATH = "#{Dir.home}/.projectr"
 	def self.list
 		dirs = Dir.entries(PROJECT_PATH).select {|f| !File.directory? f}
 		dirs.each { |dir| puts dir }
